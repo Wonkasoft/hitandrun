70cb4945603146f324374d30c9a76e2afe423a5e
llister15 <llister@wonkasoft.com>
2018-02-25 00:39:33 -0800

    added template

--- landingpage.php
+++ landingpage.php
@@ -12,7 +12,18 @@ get_header(); ?>
 		<main id="main" class="container-fluid site-main">
 			<div class="row row-for-main">
 				<div class="col above-fold-image" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>); background-size: cover;">
+					<?php
+					while ( have_posts() ) : the_post();
 
+						get_template_part( 'template-parts/page/landing', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+
+					endwhile; // End of the loop.
+					?>
 				</div> <!-- .above-fold-image -->
 			</div> <!-- .row-for-main -->
 		</main><!-- #main -->
