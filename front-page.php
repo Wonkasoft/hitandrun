3002d696cb29b8fd89126b4275cd3569c1db51db
MrLister <m_r_lister@yahoo.com>
2018-02-27 14:59:24 -0800

    fixed front page

--- front-page.php
+++ front-page.php
@@ -18,7 +18,7 @@ get_header(); ?>
 	<div id="primary" class="primary-content-area">
 		<main id="main" class="container-fluid site-main">
 			<div class="row row-for-main">
-				<div id="backdrop-image" class="col above-fold-image" style="background: url('/wp-content/uploads/2018/02/slide-1.jpg');background-position: center center">
+				<div id="backdrop-image" class="col above-fold-image" style="background: url('/wp-content/uploads/2018/02/slide-1.jpg');background-size: cover;background-position: center center">
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<?php
 			while ( have_posts() ) : the_post();
