679d8145a96bf4d4f932eca8407c82d2cf3cd1f5
llister15 <llister@wonkasoft.com>
2019-04-19 00:59:06 -0700

    update

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -95,9 +95,14 @@ add_filter( 'woocommerce_product_thumbnails_columns', 'hitandrun_woocommerce_thu
  * Function to return new placeholder image URL.
  */
 function hitandrun_custom_woocommerce_placeholder( $image_url ) {
+	// Get the custom logo id
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+  // Get the custom logo url
   $image_url = wp_get_attachment_image_src( $custom_logo_id , 'full' ); // change this to the URL to your custom placeholder
+  // Return the logo's url for the placeholder
   return $image_url;
 }
+
 add_filter( 'wc_placeholder_img_src', 'hitandrun_custom_woocommerce_placeholder', 10 );
 
 /**
